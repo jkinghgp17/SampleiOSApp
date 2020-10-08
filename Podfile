1dd8503428dd5c8cdc9c2c9ed69210e579d9ceb0
Jake King <jakeking@Jakes-MacBook-Pro.local>
2020-10-08 13:01:04 -0400

    Second Commit

--- Podfile
+++ Podfile
@@ -1,5 +1,13 @@
 source 'https://github.com/CocoaPods/Specs.git'
 
+using_bundler = defined? Bundler
+
+unless using_bundler
+    puts "\nPlease re-run using:".red
+    puts "  bundle exec pod install\n\n"
+    exit(1)
+end
+
 use_frameworks!
 
 target 'Sample-App' do
---
+++ Sample-App/ModelManager.swift
@@ -0,0 +1,67 @@
+//
+//  ModelManager.swift
+//  Sample-App
+//
+//  Created by Jake King on 10/8/20.
+//  Copyright © 2020 Jake King. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class ModelManager {
+    
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "Sample_App")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    /**
+     Access the `NSManagedObjectContext` tied to the main thread, which **must** be used for all primary user interactions.
+     
+     Cancellable user interactions should use `ModelManager.createMainContext()` to create a secondary context tied to the main thread which can be disposed of when the user interaction completes.
+     */
+    var viewContext: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+}
